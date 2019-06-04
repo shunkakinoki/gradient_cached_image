4af82eec5aab4687e77a8d79b0d8526296d77c5c
Shun Kakinoki <shunkakinoki@gmail.com>
2019-06-04 20:36:55 +0900

    init gradient_cached_image

--- lib/gradient_cached_image.dart
+++ lib/gradient_cached_image.dart
@@ -1,7 +1,60 @@
 library gradient_cached_image;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class GradientImage extends StatelessWidget {
+  GradientImage(
+      {@required this.image,
+      this.height = double.infinity,
+      this.width = double.infinity,
+      this.gradient = false,
+      this.startColor = Colors.transparent,
+      this.endColor = Colors.transparent,
+      this.fit = BoxFit.cover,
+      this.startStop = 0.6,
+      this.endStop = 1.0});
+
+  final ImageProvider image;
+  final double height;
+  final double width;
+  final bool gradient;
+  final Color startColor;
+  final Color endColor;
+  final BoxFit fit;
+  final double startStop;
+  final double endStop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Container(
+          height: height,
+          width: width,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: fit,
+                image: image,
+              ),
+              // ...
+            ),
+          )),
+      Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+              startColor,
+              endColor,
+            ],
+                stops: [
+              startStop,
+              endStop
+            ])),
+      )
+    ]);
+  }
 }
